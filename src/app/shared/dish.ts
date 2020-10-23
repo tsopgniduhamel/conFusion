88bf4b39ada88b26b7b5206156a4f1d7fbc9231e
tsopgniduhamel <tsopgniduhamel@gmail.com>
2020-10-23 09:42:03 +0100

    data binding1

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,11 +1,13 @@
-export class Dish {
-    id: string;
-    name: string;
-    image: string;
-    category: string;
-    featured: boolean;
-    label: string;
-    price: string;
-    description: string;
+import { Comment } from './comment';
 
+export class Dish {
+  id: string;
+  name: string;
+  image: string;
+  category: string;
+  featured: boolean;
+  label: string;
+  price: string;
+  description: string;
+  comments: Comment[];
 }
