dbbb0524b79987fe7d6b6c61af49bbfcf0f3bf3a
tsopgniduhamel <tsopgniduhamel@gmail.com>
2020-11-12 22:35:14 +0100

    promises first part, configuring the services to deal with the promises

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -15,7 +15,7 @@ export class MenuComponent implements OnInit {
   constructor(private dishService: DishService) {}
 
   ngOnInit(): void {
-    this.dishes = this.dishService.getDishes();
+    this.dishService.getDishes().then((dishes) => (this.dishes = dishes));
   }
 
   onSelected(dish: Dish) {
